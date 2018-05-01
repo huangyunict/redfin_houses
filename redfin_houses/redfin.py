9439e8172dfcfa0dc27db0368242d9a7a5ba25cc
Yun Huang <huangyunict@gmail.com>
2018-04-30 20:19:33 -0700

    add redfin

---
+++ redfin_houses/redfin.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+from redfin_houses.filter import HouseFilter
+
+
+class Redfin(object):
+
+    @staticmethod
+    def query_house_list(, house_filter: HouseFilter):
+        pass
