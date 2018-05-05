114924cbb985fb87abe90696e64855c3e80a00ef
Yun Huang <huangyunict@gmail.com>
2018-05-04 20:13:52 -0700

    change tag

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='redfin-houses',
-    version='0.0.2',
+    version='0.0.3',
     description='Python library to retrieve house information from Redfin',
     long_description=long_description,
     long_description_content_type='text/markdown',
